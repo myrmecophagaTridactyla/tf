6c89ced70eebe4687c7c0f08d6bfc82f522ea606
myrmecophagaTridactyla <gregorio.ortelli@gmail.com>
2018-04-06 12:02:21 +0200

    Update and rename test.py to import.py

--- import.py
+++ import.py
@@ -65,7 +65,7 @@ def _input_fn(input_filenames, num_epochs=None, shuffle=True):
   return features, labels
 
 # 50 informative terms that compose our model vocabulary 
-informative_terms = ("bad", "great", "best", "worst", "fun", "beautiful",
+'''informative_terms = ("bad", "great", "best", "worst", "fun", "beautiful",
                      "excellent", "poor", "boring", "awful", "terrible",
                      "definitely", "perfect", "liked", "worse", "waste",
                      "entertaining", "loved", "unfortunately", "amazing",
@@ -77,35 +77,4 @@ informative_terms = ("bad", "great", "best", "worst", "fun", "beautiful",
                      "drama", "family")
 
 terms_feature_column = tf.feature_column.categorical_column_with_vocabulary_list(key="terms", vocabulary_list=informative_terms)
-
-my_optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
-my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
-
-feature_columns = [ terms_feature_column ]
-
-
-classifier = tf.estimator.LinearClassifier(
-  feature_columns=feature_columns,
-  optimizer=my_optimizer,
-)
-
-classifier.train(
-  input_fn=lambda: _input_fn([train_path]),
-  steps=1000)
-
-evaluation_metrics = classifier.evaluate(
-  input_fn=lambda: _input_fn([train_path]),
-  steps=1000)
-print "Training set metrics:"
-for m in evaluation_metrics:
-  print m, evaluation_metrics[m]
-print "---"
-
-evaluation_metrics = classifier.evaluate(
-  input_fn=lambda: _input_fn([test_path]),
-  steps=1000)
-
-print "Test set metrics:"
-for m in evaluation_metrics:
-  print m, evaluation_metrics[m]
-print "---"
+'''
