cb674418c4893b51184fa29796a7d1d7976e4d7c
myrmecophagaTridactyla <gregorio.ortelli@gmail.com>
2018-04-06 12:27:21 +0200

    Update linearClassif.py

--- linearClassif.py
+++ linearClassif.py
@@ -1,6 +1,7 @@
 periods = 10
 steps = 1000
 steps_per_period = steps / periods  
+feature_columns = [ terms_feature_column ]
 
 my_optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
 my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
